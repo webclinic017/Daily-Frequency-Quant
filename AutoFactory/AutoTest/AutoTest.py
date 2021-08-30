b4eac72586c6c587f2bb01fe61d51442ef5d2415
GYMS-PKU <18316887409@163.com>
2021-08-30 20:58:47 +0800

    new AutoTest and DataLoader

---
+++ AutoFactory/AutoTest/AutoTest.py
@@ -0,0 +1,46 @@
+# Copyright (c) 2021 Dai HBG
+
+"""
+该代码定义的类用于计算一个信号的平均IC等统计值
+This code is used for calculating the average IC and other statistical values of a signal
+
+v1.0
+2021-08-30
+-- 定义：计算平均IC，信号自相关，理论换手率
+"""
+
+import numpy as np
+
+
+class Stats:
+    def __init__(self):
+        self.ICs = []
+        self.mean_IC = 0
+        self.auto_corr = 0
+
+
+class AutoTest:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def test(signal, ret, orders):
+        ics = []
+        auto_corr = []
+        assert len(signal) == len(ret)
+        assert len(signal) == len(orders)
+        for i in range(len(signal)):
+            ics.append(np.corrcoef(signal[i, orders[i]], ret[i, orders[i]])[0, 1])
+            if i >= 1:
+                auto_corr.append(
+                    np.corrcoef(signal[i, orders[i] & orders[i - 1]], ret[i, orders[i] & orders[i - 1]])[0, 1])
+
+        ics = np.array(ics)
+        ics[np.isnan(ics)] = 0
+        auto_corr = np.array(auto_corr)
+        auto_corr[np.isnan(auto_corr)] = 0
+        stats = Stats()
+        stats.ICs = ics
+        stats.mean_IC = np.mean(ics)
+        stats.auto_corr = np.mean(auto_corr)
+        return stats
--- AutoFactory/DataLoader/DataLoader.py
+++ AutoFactory/DataLoader/DataLoader.py
@@ -3,6 +3,11 @@
 """
 该代码定义的类用于调用聚宽的接口查询数据
 This code is used for calling the interface of Join Quant to query data
+
+v1.0
+2021-08-30
+-- 暂时不支持分钟数据，因为聚宽的数据查询有次数限制
+-- get_pv_data方法的参数data_type传入一个列表，表示需要的数据
 """
 
 import numpy as np
@@ -11,6 +16,7 @@ import pandas as pd
 import pickle
 import jqdatasdk
 from jqdatasdk import *
+import datetime
 
 
 class DataLoader:
@@ -20,23 +26,40 @@ class DataLoader:
         self.password = password
         jqdatasdk.auth(self.user_id, self.password)  # 登录聚宽
 
-    def get_pv_data(self, date, data_type='daily'):  # 获得日频量价关系数据
-        all_stocks = list(get_all_securities(types=['stock'], date=None).index)
-        if data_type == 'daily':
-            all_indexes = get_all_securities(types=['stock'], date=None)
+    def get_pv_data(self, date, data_type=None):  # 获得日频量价关系数据
+        if data_type is None:  # 参数默认值不要是可变的，否则可能出错
+            data_type = ['stock_daily']
+
+        if 'stock_daily' in data_type:
+            all_stocks = list(get_all_securities(types=['stock'], date=date).index)
             lst = os.listdir('{}/StockDailyData'.format(self.data_path))
             if date not in lst:
                 os.makedirs('{}/StockDailyData/{}'.format(self.data_path, date))
-            with open('{}/StockDailyData/{}/index_{}.pkl'.format(self.data_path, date, date), 'wb') as f:
-                pickle.dump(all_indexes, f)
-            stock_data = get_price(all_stocks,frequency='daily',date=date)
+            day = date.split('-')
+            end_date = str(datetime.date(int(day[0]), int(day[1]), int(day[2])).timedelta(days=1))
+            stock_data = get_price(all_stocks, frequency='daily',
+                                   field=['open', 'close', 'low', 'high', 'volume', 'money', 'pre_close', 'factor'],
+                                   start_date=date, end_date=end_date)
             with open('{}/StockDailyData/{}/stock_{}.pkl'.format(self.data_path, date, date), 'wb') as f:
                 pickle.dump(stock_data, f)
-        elif data_type == 'minute':
+        if 'index_daily' in data_type:
+            all_indexes = get_all_securities(types=['index'], date=date)
+            with open('{}/StockDailyData/{}/index_{}.pkl'.format(self.data_path, date, date), 'wb') as f:
+                pickle.dump(all_indexes, f)
+
+        if 'minute' in data_type:
             lst = os.listdir('{}/StockIntraDayData'.format(self.data_path))
             if date not in lst:
                 os.makedirs('{}/StockIntraDayData/{}'.format(self.data_path, date))
-                for stock in all_stocks:
-                    intradaydata = get_price(stock,frequency='minute',date=date)
+                all_stocks = list(get_all_securities(types=['stock'], date=date).index)
+                for stock in all_stocks:  # 剔除创业板股票，避免超出查询限制
+                    if stock[:3] == '003':
+                        continue
+                    day = date.split('-')
+                    end_date = str(datetime.date(int(day[0]), int(day[1]), int(day[2])).timedelta(days=1))
+                    intra_day_data = get_price(stock, frequency='minute',
+                                               field=['open', 'close', 'low', 'high', 'volume', 'money', 'pre_close',
+                                                      'factor'],
+                                               start_date=date, end_date=end_date)
                     with open('{}/StockIntraDayData/{}/{}.pkl'.format(self.data_path, date, stock), 'wb') as f:
-                        pickle.dump(intradaydata, f)
+                        pickle.dump(intra_day_data, f)
