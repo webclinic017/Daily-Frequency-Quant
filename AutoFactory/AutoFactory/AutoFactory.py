dba3f13c502d6107893dc21dd10b3d36b3cff936
GYMS-PKU <18316887409@163.com>
2021-08-31 19:39:33 +0800

    new DataLoader

--- AutoFactory/AutoFactory/AutoFactory.py
+++ AutoFactory/AutoFactory/AutoFactory.py
@@ -1,1 +1,6 @@
 # Copyright (c) 2021 Dai HBG
+
+"""
+
+
+"""
--- AutoFactory/DataLoader/DataLoader.py
+++ AutoFactory/DataLoader/DataLoader.py
@@ -32,6 +32,7 @@ class DataLoader:
     """
     get_pv_data定义了从聚宽读取量价数据(pv for Price & Volume)并保存在本地的方法
     """
+
     def get_pv_data(self, date, data_type=None):  # 获得日频量价关系数据
         if data_type is None:  # 参数默认值不要是可变的，否则可能出错
             data_type = ['stock_daily']
@@ -77,44 +78,70 @@ class DataLoader:
     2021-08-30
     1. 需要解析一个字段，告诉DataLoader应该取从什么地方到什么地方的收益率，例如开盘价收益率或者日内收益率，周收益率等
     """
-    def get_matrix_data(self, back_test_name='default',
+
+    def get_matrix_data(self, back_test_name='default', frequency='daily',
                         start_date='2021-01-01', end_date='2021-06-30'):  # 读入dataframe数据另存为便于处理的矩阵形式
         tmp = start_date.split('-')
-        start_date = datetime.date(int(tmp[0]), int(tmp[1]), int(tmp[2]))
+        start_date = datetime.date(int(tmp[0]), int(tmp[1]), int(tmp[2]))  # 回测开始时间
         tmp = end_date.split('-')
-        end_date = datetime.date(int(tmp[0]), int(tmp[1]), int(tmp[2]))
+        end_date = datetime.date(int(tmp[0]), int(tmp[1]), int(tmp[2]))  # 回测结束时间
+        if frequency == 'daily':  # 当前仅支持日频的回测
+            lst = os.listdir('{}'.format(self.back_test_data_path))
+            if back_test_name not in lst:
+                os.makedirs('{}/{}'.format(self.back_test_data_path, back_test_name))
+            lst = os.listdir('{}/{}'.format(self.back_test_data_path, back_test_name))
+            if 'code_order_dic.pkl' not in lst:  # code_order_dic用于存储该回测区间内出现过的股票代码到矩阵位置的映射
+                dates = os.listdir('{}/StockDailyData'.format(self.data_path))
+                codes_order_dic = {}
+                order = 0
+                days = 1
+                for i in range((end_date - start_date).days + 1):
+                    date = start_date + datetime.timedelta(days=i)
+                    if str(date) in dates:  # 要加入判断今天是否全市场停牌，方法是检测相邻两天是不是完全一致
+                        with open('{}/StockDailyData/{}/stock_{}.pkl'.format(self.data_path,
+                                                                             date, date), 'rb') as file:
+                            data = pickle.load(file)
+                            codes = list(data.index)
+                            for code in codes:
+                                try:
+                                    codes_order_dic[code]
+                                except KeyError as e:  # 代码规范：最好写明具体的错误类型
+                                    codes_order_dic[code] = order
+                                    order += 1
+                        days += 1
+                with open('{}/{}/code_order_dic.pkl'.format(self.back_test_data_path, back_test_name), 'wb') as f:
+                    pickle.dump(codes_order_dic, f)
+            else:
+                with open('{}/{}/code_order_dic.pkl'.format(self.back_test_data_path, back_test_name), 'rb') as f:
+                    codes_order_dic = pickle.load(f)
+                dates = os.listdir('{}/StockDailyData'.format(self.data_path))
+                days = 1
+                for i in range((end_date - start_date).days + 1):
+                    date = start_date + datetime.timedelta(days=i)
+                    if str(date) in dates:
+                        days += 1
 
-        lst = os.listdir('{}'.format(self.back_test_data_path)
-        if code_order_dic not in lst:  # code_order_dic用于存储该回测区间内出现过的股票代码到矩阵位置的映射
-            dates = os.listdir(date_dir)
-        codes_order_dic = {}
-        order = 0
-        days = 1
-        for i in range((end_date - start_date).days + 1):
-            date = begin + datetime.timedelta(days=i)
-        if str(date) in dates:  # 要加入判断今天是否全市场停牌，方法是检测相邻两天是不是完全一致
-            with
-        open(date_dir + '/{}/stock_{}.pkl'.format(date, date), 'rb') as file:
-        data = pickle.load(file)
-        codes = list(data.index)
-        for code in codes:
-            try:
-                num = codes_order_dic[code]
-            except:
-                codes_order_dic[code] = order
-                order += 1
-        days += 1
+            names = ['open', 'close', 'high', 'low', 'volume', 'money']
+            data_dic = {}
+            for name in names:
+                data_dic[name] = np.zeros((days, len(codes_order_dic)))
 
-        close = np.zeros((days, len(codes_order_dic)))
-        ret = np.zeros((days, len(codes_order_dic)))
-        k = 0
-        for i in range((end_date - start_date).days + 1):
-            date = begin + datetime.timedelta(days=i)
-            if str(date) in dates:
-                with open(date_dir + '/{}/stock_{}.pkl'.format(date, date), 'rb') as file:
-                    data = pickle.load(file)
-                    index = list(data.index)
-                    for j in range(len(data)):
-                        close[k, codes_order_dic[index[j]]] = data['close'][j]
-                        ret[k, codes_order_dic[index[j]]] = data['close'][j] / data['open'][j] - 1
-                    k += 1
+            ret = np.zeros((days, len(codes_order_dic)))
+
+            k = 0
+            for i in range((end_date - start_date).days + 1):
+                date = start_date + datetime.timedelta(days=i)
+                if str(date) in dates:
+                    with open('{}/StockDailyData/{}/stock_{}.pkl'.format(self.data_path,
+                                                                         date, date), 'rb') as file:
+                        data = pickle.load(file)
+                        index = list(data.index)
+                        for j in range(len(data)):
+                            for name in names:
+                                data_dic[name][k, codes_order_dic[index[j]]] = data[name][j]
+                            ret[k, codes_order_dic[index[j]]] = data['close'][j] / data['open'][j] - 1  # 需要改成定制的收益率
+                        k += 1
+            with open('{}/{}/raw_data_dic.pkl'.format(self.back_test_data_path, back_test_name), 'wb') as f:
+                pickle.dump(data_dic, f)
+            with open('{}/{}/return.pkl'.format(self.back_test_data_path, back_test_name), 'wb') as f:
+                pickle.dump(ret, f)
